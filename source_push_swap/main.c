095cdf314a58d99485db63ebe16c1aff697b6fed
MixFon <mixfon@rambler.ru>
2020-01-15 17:00:09 +0300

    Push_swap

--- source_push_swap/main.c
+++ source_push_swap/main.c
@@ -54,7 +54,6 @@ int		check_number(char *arg)
 		ft_printf("Arguments %ld is not an integer type.\n", data);
 		exit(0);
 	}
-	//ft_printf("data = [%ld]\n", data);
 	return ((int)data);
 }
 
@@ -70,6 +69,16 @@ t_node	*create_node(int data, size_t *count)
 	return (node);
 }
 
+void	search_dublicate(t_node *node, int data)
+{
+	while (node)
+	{
+		if (node->data == data)
+			sys_err("Found dublicate.\n");
+		node = node->next;
+	}
+}
+
 void	add_node(t_ps *ps, int data)
 {
 	t_node *tmp;
@@ -80,21 +89,34 @@ void	add_node(t_ps *ps, int data)
 		ps->stack_a.bott = ps->stack_a.top;
 		return ;
 	}
+	search_dublicate(ps->stack_a.top, data);
 	tmp = create_node(data, &ps->stack_a.count);
 	ps->stack_a.bott->next = tmp;
 	ps->stack_a.bott = tmp;
 }
 
-void	print_node(t_stack *stack)
+void	print_node(t_stack *stack_a, t_stack *stack_b)
 {
-	t_node *node;
+	t_node *node_a;
+	t_node *node_b;
 
-	node = stack->top;
-	ft_printf("node->count = {%d}\n", stack->count);
-	while (node)
+	node_a = stack_a->top;
+	node_b = stack_b->top;
+	ft_printf("\nnode->count = {%d}\n", stack_a->count);
+	while (node_a || node_b)
 	{
-		ft_printf("node->data = {%d}\n", node->data);
-		node = node->next;
+		if (node_a)
+			ft_printf("{%d}\t", node_a->data);
+		else
+			ft_printf("{%c}\t", ' ');
+		if (node_b)
+			ft_printf("{%d}\n", node_b->data);
+		else
+			ft_printf("{%c}\n", ' ');
+		if (node_a)
+			node_a = node_a->next;
+		if (node_b)
+			node_b = node_b->next;
 	}
 }
 
@@ -107,7 +129,82 @@ void	infill_stack(t_ps *ps, char **av)
 		check_arg(*av);
 		add_node(ps, check_number(*av));
 	}
-	print_node(&ps->stack_a);
+	print_node(&ps->stack_a, &ps->stack_b);
+}
+
+void	op_swap(t_stack *stack)
+{
+	t_node *node;
+
+	if (stack->count < 2)
+		return ;
+	node = stack->top->next;
+	stack->top->next = node->next; 
+	node->next = stack->top;
+	stack->top = node;
+	if (stack->top == stack->bott)
+		stack->bott = stack->top->next;
+}
+
+void	op_rotate(t_stack *stack)
+{
+	t_node *node;
+
+	if (stack->count < 2)
+		return ;
+	node = stack->top;
+	stack->top = stack->top->next;
+	stack->bott->next = node;
+	stack->bott = node;
+	node->next = NULL;
+	//ft_printf("top->data = {%d}\n", stack->top->data);
+	//ft_printf("bott->data = {%d}\n", stack->bott->data);
+}
+
+void	op_reverse_rotate(t_stack *stack)
+{
+	t_node *node;
+
+	if (stack->count < 2)
+		return ;
+	node = stack->top;
+	while (node->next != stack->bott)
+		node = node->next;
+	node->next = NULL;
+	stack->bott->next = stack->top;
+	stack->top = stack->bott;
+	stack->bott = node;
+	ft_printf("top->data = {%d}\n", stack->top->data);
+	ft_printf("bott->data = {%d}\n", stack->bott->data);
+}
+
+void	op_push(t_stack *one, t_stack *two)
+{
+	t_node *node;
+
+	if (one->count == 0)
+		return ;
+	node = one->top;
+	one->top = one->top->next;
+	one->count--;
+	node->next = two->top;
+	ft_putendl("111");
+	two->top->next = node;
+	ft_putendl("111");
+	two->count++;
+	ft_putendl("111");
+}
+
+void	operations(t_ps *ps)
+{
+	op_swap(&ps->stack_a);
+	print_node(&ps->stack_a, &ps->stack_b);
+	op_rotate(&ps->stack_a);
+	print_node(&ps->stack_a, &ps->stack_b);
+	op_reverse_rotate(&ps->stack_a);
+	print_node(&ps->stack_a, &ps->stack_b);
+	op_push(&ps->stack_a, &ps->stack_b);
+	print_node(&ps->stack_a, &ps->stack_b);
 }
 
 int		main(int ac, char **av)
@@ -118,5 +215,6 @@ int		main(int ac, char **av)
 		sys_err("To few arguments.\n");
 	init(&ps);
 	infill_stack(&ps, av);
+	operations(&ps);
 	return (0);
 }
